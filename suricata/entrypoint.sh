6388238d271197188260caf4afecc63949d76796
aiooss-anssi <alexandre.iooss@ssi.gouv.fr>
2024-01-16 15:59:29 +0100

    suricata/entrypoint.sh: add PCAP_FILE env

--- suricata/entrypoint.sh
+++ suricata/entrypoint.sh
@@ -5,5 +5,9 @@
 echo "Cleaning previous Suricata output"
 rm -f suricata/output/eve.json suricata/output/tcpstore.log suricata/output/udpstore.log
 
-echo "Starting Suricata with HOME_NET=$CTF_HOME_NET"
-suricata -c suricata/suricata.yaml -r input_pcaps -l suricata/output --set vars.address-groups.HOME_NET="$CTF_HOME_NET" --runmode=single --no-random --pcap-file-continuous
+echo "Starting Suricata with HOME_NET=$CTF_HOME_NET PCAP_FILE=$PCAP_FILE"
+suricata -c suricata/suricata.yaml -r input_pcaps -l suricata/output \
+    --set vars.address-groups.HOME_NET="${CTF_HOME_NET}" \
+    --set outputs.1.eve-log.pcap-file=${PCAP_FILE:=true} \
+    --runmode=single --no-random \
+    --pcap-file-continuous
