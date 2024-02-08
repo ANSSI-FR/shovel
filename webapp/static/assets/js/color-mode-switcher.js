24cf5e307a28d5f4e7da80bb99f225e5b0583ddb
aiooss-anssi <alexandre.iooss@ssi.gouv.fr>
2024-02-08 16:46:24 +0100

    webapp: implement color mode switcher

---
+++ webapp/static/assets/js/color-mode-switcher.js
@@ -0,0 +1,37 @@
+'use strict'
+/* eslint-env browser */
+
+/**
+ * Get user preferred theme from their past choice or browser
+ * @returns {String} User preferred theme
+ */
+function getPreferredTheme () {
+  const storedTheme = localStorage.getItem('theme')
+  if (storedTheme) {
+    return storedTheme
+  }
+  // Privacy-hardened browsers always return light
+  return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+}
+
+// Change body theme early to prevent flash
+let currentTheme = getPreferredTheme()
+document.documentElement.setAttribute('data-bs-theme', currentTheme)
+
+// On browser color-scheme change, update
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+  currentTheme = getPreferredTheme()
+  document.documentElement.setAttribute('data-bs-theme', currentTheme)
+})
+
+window.addEventListener('load', () => {
+  // Toggle on T key, ignore input fields or key repeat
+  document.addEventListener('keydown', e => {
+    if (e.target.tagName !== 'INPUT' && !e.repeat && !e.ctrlKey && e.key === 't') {
+      currentTheme = currentTheme === 'light' ? 'dark' : 'light'
+      document.documentElement.setAttribute('data-bs-theme', currentTheme)
+      localStorage.setItem('theme', currentTheme)
+      e.preventDefault()
+    }
+  })
+})
