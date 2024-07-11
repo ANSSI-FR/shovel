86052acb4a8303297cef66d604d386aaf16ce817
aiooss-anssi <alexandre.iooss@ssi.gouv.fr>
2024-07-11 11:10:28 +0200

    suricata: remove HOME_NET and add CINI hex rules

--- suricata/entrypoint.sh
+++ suricata/entrypoint.sh
@@ -9,11 +9,10 @@ SURICATA_PARAM="--runmode=single --no-random"
 if [ "${PCAP_FILE_CONTINUOUS:=true}" = true ]; then
     SURICATA_PARAM="${SURICATA_PARAM} --pcap-file-continuous"
 fi
-echo "Starting Suricata with HOME_NET=${CTF_HOME_NET:=[]} PCAP_FILE=${PCAP_FILE:=true} PCAP_FILE_CONTINUOUS=${PCAP_FILE_CONTINUOUS:=true}"
+echo "Starting Suricata with PCAP_FILE=${PCAP_FILE:=true} PCAP_FILE_CONTINUOUS=${PCAP_FILE_CONTINUOUS:=true}"
 suricata -r input_pcaps \
     -S suricata/rules/suricata.rules \
     -l suricata/output \
-    --set vars.address-groups.HOME_NET="${CTF_HOME_NET:=[]}" \
     --set plugins.0=suricata/libeve_sqlite_output.so \
     --set outputs.0.fast.enabled=no \
     --set outputs.1.eve-log.filetype=sqlite \
